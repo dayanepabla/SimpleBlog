5fd88a3573d96a17136354b90b220d1b88996876
dayanepabla <dayanepabla@gmail.com>
2015-07-01 17:39:23 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# SimpleBlog
+SimpleBlog
+==========
+
 Blog com interface simples e intuitiva.
+
+## Preparando o ambiente de desenvolvimento
+
+### Requisitos
+Antes de proceder com a intalação, certifique-se de que os requisitos abaixo são satisfeitos:
+
+- Node.js e NPM
+- Gulp
+
+### Instalação
+1) Clone o repositório conforme exposto abaixo:
+
+```
+$ git clone https://github.com/dayanepabla/SimpleBlog.git
+$ cd SimpleBlog
+```
+
+2) Instale todas as dependências do projeto
+
+```
+$ npm install
+```
+
+3) Concluída a instalação de todas as dependências, execute o comando `gulp` para verificar se tudo está funcionando corretamente.
+
+4) Inicialize o servidor de desenvolvimento
+
+```
+$ gulp serve
+```
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var paths = {
+  build: 'dist',
+  styles: 'src/css/**/*.css',
+  scripts: 'src/js/**/*.js',
+  images: 'src/img/**/*',
+  html: 'src/*.html',
+};
+
+gulp.task('default', ['html', 'images', 'styles', 'scripts'], function() {});
+
+gulp.task('clean', function(cb) {
+  del([paths.build], cb);
+});
+
+gulp.task('html', function() {
+  return gulp.src(paths.html)
+             .pipe($.minifyHtml())
+             .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('images', function() {
+  var destPath = paths.build + '/img';
+
+  return gulp.src(paths.images)
+             .pipe($.cache($.imagemin({
+                progressive: true,
+                interlaced: true
+             })))
+             .pipe(gulp.dest(destPath));
+});
+
+gulp.task('styles', function() {
+  var destPath = paths.build + '/css';
+
+  return gulp.src(paths.styles)
+             .pipe($.concat('site.css'))
+             .pipe($.minifyCss())
+             .pipe(gulp.dest(destPath));
+});
+
+gulp.task('scripts', function() {
+  var destPath = paths.build + '/js';
+
+  return gulp.src(paths.scripts)
+             .pipe($.concat('site.js'))
+             .pipe($.stripDebug())
+             .pipe($.uglify())
+             .pipe(gulp.dest(destPath));
+});
+
+gulp.task('serve', function() {
+  browserSync({
+    notify: false,
+    logPrefix: 'Server',
+    server: ['src']
+  });
+
+  gulp.watch(paths.html, ['html', reload]);
+  gulp.watch(paths.styles, ['styles', reload]);
+  gulp.watch(paths.images, ['images', reload]);
+  gulp.watch(paths.scripts, ['scripts', reload]);
+});
+
+gulp.task('serve:dist', ['default'], function() {
+  browserSync({
+    notify: false,
+    logPrefix: 'Server:Dist',
+    server: ['dist']
+  });
+});
